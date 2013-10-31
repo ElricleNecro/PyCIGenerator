88b0897998d01b329472b9bd039de2e9146c1f53
ElricleNecro <guiplum@gmail.com>
2013-10-31 11:27:27 +0100

    Enhanced usability of the code. Use a dictionnary for fields instead of class members.

--- InitialCond/Gadget/Config.py
+++ InitialCond/Gadget/Config.py
@@ -2,64 +2,143 @@
 # -*- coding:Utf8 -*-
 
 class Config(object):
-	Param    = dict()
-	filename = None
+	Param      = dict()
+	Categories = dict(
+			Time   = ["TimeOfFirstSnapshot",
+				  "TimeBegin",
+				  "TimeMax",
+				  "TimeBetSnapshot",
+				  "TimeBetStatistics",
+				  "MinSizeTimestep",
+				  "MaxSizeTimestep"
+				],
+			Units  = ["UnitLength_in_cm",
+				  "UnitMass_in_g",
+				  "UnitVelocity_in_cm_per_s",
+				  "GravityConstantInternal"
+				],
+			Soft   = ["SofteningGas",
+				  "SofteningGasMaxPhys",
+				  "SofteningHalo",
+				  "SofteningHaloMaxPhys",
+				  "SofteningDisk",
+				  "SofteningDiskMaxPhys",
+				  "SofteningBulge",
+				  "SofteningBulgeMaxPhys",
+				  "SofteningStars",
+				  "SofteningStarsMaxPhys",
+				  "SofteningBndry",
+				  "SofteningBndryMaxPhys"],
+			Files  = ["InitCondFile",
+				  "ICFormat",
+				  "SnapshotFileBase",
+				  "SnapFormat",
+				  "NumFilesPerSnapshot",
+				  "NumFilesWrittenInParallel",
+				  "OutputDir",
+				  "OutputListOn",
+				  "OutputListFilename",
+				  "RestartFile",
+				  "CpuTimeBetRestartFile",
+				  "EnergyFile",
+				  "InfoFile",
+				  "TimingsFile",
+				  "CpuFile"
+				],
+			Cosmo  = ["Omega0",
+				  "OmegaBaryon",
+				  "OmegaLambda",
+				  "HubbleParam"
+				],
+			Conf   = ["ResubmitOn",
+				  "ResubmitCommand",
+				  "TypeOfOpeningCriterion",
+				  "TypeOfTimestepCriterion",
+				  "ErrTolTheta",
+				  "ErrTolIntAccuracy",
+				  "ErrTolForceAcc",
+				  "TreeDomainUpdateFrequency",
+				  "TreeAllocFactor",
+				  "BufferSize",
+				  "PartAllocFactor"
+				  "CourantFac",
+				  "MaxRMSDisplacementFac",
+				],
+			SParam = ["BoxSize",
+				  "PeriodicBoundariesOn",
+				  "ComovingIntegrationOn",
+				  "MaxNumNgbDeviation"
+				],
+			Physic = ["MinGasTemp",
+				  "MinGasHsmlFractional",
+				  "InitGasTemp",
+				  "ArtBulkViscConst"
+				]
+		     )
+	filename   = None
 	def __init__(self, filename):
 		self.filename = filename
 
 		self.Param["InitCondFile"]              = None
+		self.Param["ICFormat"]                  = 1
 		self.Param["OutputDir"]                 = None
+		self.Param["SnapshotFileBase"]          = None
+		self.Param["SnapFormat"]                = 1
+		self.Param["NumFilesPerSnapshot"]       = 1
+		self.Param["NumFilesWrittenInParallel"] = 1
+		self.Param["RestartFile"]               = "restart"
+		self.Param["CpuTimeBetRestartFile"]     = 72000
+		self.Param["OutputListOn"]              = 0
+		self.Param["OutputListFilename"]        = None
+
 		self.Param["EnergyFile"]                = "energy.log"
 		self.Param["InfoFile"]                  = "info.log"
 		self.Param["TimingsFile"]               = "timings.log"
 		self.Param["CpuFile"]                   = "cpu.log"
-		self.Param["RestartFile"]               = "restart"
-		self.Param["SnapshotFileBase"]          = None
-		self.Param["OutputListFilename"]        = None
-		self.Param["NumFilesPerSnapshot"]       = 1
-		self.Param["NumFilesWrittenInParallel"] = 1
-		self.Param["ICFormat"]                  = 1
-		self.Param["SnapFormat"]                = 1
 		self.Param["TimeLimitCPU"]              = 72000
 		self.Param["ResubmitOn"]                = 0
 		self.Param["ResubmitCommand"]           = None
-		self.Param["CpuTimeBetRestartFile"]     = 72000
+
+		self.Param["BoxSize"]                   = 0.
 		self.Param["ComovingIntegrationOn"]     = 0
 		self.Param["TypeOfTimestepCriterion"]   = 0
-		self.Param["OutputListOn"]              = 0
 		self.Param["PeriodicBoundariesOn"]      = 0
+
 		self.Param["TimeBegin"]                 = 0.0
 		self.Param["TimeMax"]                   = 1.0
+		self.Param["TimeBetSnapshot"]           = 0.1
+		self.Param["TimeOfFirstSnapshot"]       = 0.1
+		self.Param["TimeBetStatistics"]         = 0.05
+		self.Param["MaxSizeTimestep"]           = 1e-5
+		self.Param["MinSizeTimestep"]           = 0.
+
 		self.Param["Omega0"]                    = 0.
 		self.Param["OmegaLambda"]               = 0.
 		self.Param["OmegaBaryon"]               = 0.
 		self.Param["HubbleParam"]               = 0.
-		self.Param["BoxSize"]                   = 0.
-		self.Param["TimeBetSnapshot"]           = 0.1
-		self.Param["TimeOfFirstSnapshot"]       = 0.1
-		self.Param["TimeBetStatistics"]         = 0.05
-		self.Param["ErrTolIntAccuracy"]         = 0.025
+
+		self.Param["InitGasTemp"]               = 0
+		self.Param["MinGasTemp"]                = 0
+		self.Param["MinGasHsmlFractional"]      = 0.25
 		self.Param["MaxRMSDisplacementFac"]     = 0.2
 		self.Param["CourantFac"]                = 0.15
-		self.Param["MaxSizeTimestep"]           = 1e-5
-		self.Param["MinSizeTimestep"]           = 0.
 		self.Param["ErrTolTheta"]               = 0.5
-		self.Param["TypeOfOpeningCriterion"]    = 1
 		self.Param["ErrTolForceAcc"]            = 0.005
+		self.Param["ErrTolIntAccuracy"]         = 0.025
+		self.Param["TypeOfOpeningCriterion"]    = 1
 		self.Param["TreeDomainUpdateFrequency"] = 0.1
 		self.Param["DesNumNgb"]                 = 0
 		self.Param["ArtBulkViscConst"]          = 0.0
 		self.Param["MaxNumNgbDeviation"]        = 0
-		self.Param["InitGasTemp"]               = 0
-		self.Param["MinGasTemp"]                = 0
 		self.Param["PartAllocFactor"]           = 30.0
 		self.Param["TreeAllocFactor"]           = 5.0
 		self.Param["BufferSize"]                = 30
+
 		self.Param["UnitLength_in_cm"]          = None
 		self.Param["UnitMass_in_g"]             = None
 		self.Param["UnitVelocity_in_cm_per_s"]  = None
-		self.Param["GravityConstantInternal"]   = 0
-		self.Param["MinGasHsmlFractional"]      = 0.25
+		self.Param["GravityConstantInternal"]   = 1
+
 		self.Param["SofteningGas"]              = None
 		self.Param["SofteningHalo"]             = None
 		self.Param["SofteningDisk"]             = None
@@ -87,7 +166,7 @@ class Config(object):
 	def __str__(self):
 		res = str()
 		for key in gadget_cfg.Param:
-			res += "%025s"%(key) + str(gadget_cfg.Param[key]) + "\n"
+			res += "%025s"%(key) + " " + str(gadget_cfg.Param[key]) + "\n"
 		return res
 
 	def __repr__(self):
